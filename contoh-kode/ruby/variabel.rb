78120a89d08411c2f030fa625c4639715e6ece9b
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-09 16:01:28 +0700

    contoh deklarasi variabel

---
+++ contoh-kode/ruby/variabel.rb
@@ -0,0 +1,14 @@
+nama = "endy";
+puts(nama);
+
+# syntactic sugar = cara penulisan yang lebih nyaman
+# nyaman != mudah dipahami (bagi programmer baru)
+# tanda kurung dalam pemanggilan function boleh dihilangkan
+
+puts nama;
+
+# string replacement, memproses yang di dalam tanda kurung
+# kemudian menghasilkan string
+
+puts "Halo #{nama}";
+puts "2 + 1 = #{2+1}";
