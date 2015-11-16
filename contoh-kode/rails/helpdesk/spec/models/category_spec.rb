de6478c14538db9b7994992604e8eaf00abf71e0
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-16 14:54:28 +0700

    convert should to expect

--- contoh-kode/rails/helpdesk/spec/models/category_spec.rb
+++ contoh-kode/rails/helpdesk/spec/models/category_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe 'Category' do
     it 'has a valid factory' do
-        create(:category).should be_valid
+        expect(create(:category)).to be_valid
     end
 
     it 'is invalid without code' do
-        build(:category, code: nil).should_not be_valid
+        expect(build(:category, code: nil)).not_to be_valid
     end
 
     it 'is invalid without name' do
-        build(:category, name: nil).should_not be_valid
+        expect(build(:category, name: nil)).not_to be_valid
     end
 end
