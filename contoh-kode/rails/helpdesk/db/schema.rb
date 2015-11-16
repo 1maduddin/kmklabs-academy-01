529ddb4787eddb1ff6a0842a271021ebf56a23b4
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-16 15:37:10 +0700

    remove table user

--- contoh-kode/rails/helpdesk/db/schema.rb
+++ contoh-kode/rails/helpdesk/db/schema.rb
@@ -20,11 +20,4 @@ ActiveRecord::Schema.define(version: 20151116070637) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
