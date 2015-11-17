f0cc51ff0ec4cda7d8c96d352967949c6dcfba0f
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-17 15:26:42 +0700

    add test asosiasi untuk comment

--- contoh-kode/rails/helpdesk/spec/models/comment_spec.rb
+++ contoh-kode/rails/helpdesk/spec/models/comment_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Comment, type: :model do
     it "creates valid comment" do
         expect(build(:comment)).to be_valid
     end
+
+    it "has valid association" do
+        FactoryGirl.reload
+        c = create(:comment)
+        expect(c.problem.title).to eql "Sample Problem"
+        expect(c.problem.user.email).to eql "user1@geemail.com"
+        expect(c.problem.product.name).to eql "Aplikasi 001"
+    end
 end
