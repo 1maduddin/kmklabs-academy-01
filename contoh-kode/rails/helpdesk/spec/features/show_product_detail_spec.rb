3b34eb9c447e30da3ae1e72a665dae8886655cd8
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-19 11:19:28 +0700

    testing menggunakan xpath

--- contoh-kode/rails/helpdesk/spec/features/show_product_detail_spec.rb
+++ contoh-kode/rails/helpdesk/spec/features/show_product_detail_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 feature 'Display Product Detail' do
     scenario 'product with the specified id exists in database' do
-        visit 'products/10'
-        expect(page).to have_content 'Code'
+        visit products_path
+        link_product = page.find(:xpath, "//tbody/tr[5]/td[1]/a")
+        product_code = page.find(:xpath, "//tbody/tr[5]/td[2]").native.text
+        link_product.click
+        expect(page).to have_content product_code
     end
 
     scenario 'product not exists in database' do
