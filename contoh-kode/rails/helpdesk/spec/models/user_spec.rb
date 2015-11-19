249fffc9034e75f1d04f86b77b8d059dfb619126
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-19 15:15:25 +0700

    hash password dengan bcrypt

--- contoh-kode/rails/helpdesk/spec/models/user_spec.rb
+++ contoh-kode/rails/helpdesk/spec/models/user_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe User, type: :model do
         user = build(:user, :with_special_chars_in_email)
         expect(user).to be_valid
     end
+    
+    it "has secure password" do
+        user = build(:user, password: nil)
+        expect(user.password_digest).to be_nil
+
+        user.password = "1234"
+        expect(user.password_digest).not_to be_nil
+        user.password_confirmation = "123"
+        expect(user.valid?).to be_falsey
+
+        user.password_confirmation = "1234"
+        expect(user.valid?).to be_truthy
+
+        user.save
+    end
 end
