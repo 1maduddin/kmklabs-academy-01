9820db8307a149ea43c74a4514b2cbb10a373a99
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-24 16:25:23 +0700

    update vendor libs

--- contoh-kode/laravel/helpdesk/vendor/composer/autoload_classmap.php
+++ contoh-kode/laravel/helpdesk/vendor/composer/autoload_classmap.php
@@ -6,7 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateCommentsTable' => $baseDir . '/database/migrations/2015_11_24_091443_create_comments_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateProblemsTable' => $baseDir . '/database/migrations/2015_11_24_091138_create_problems_table.php',
     'CreateProductsTable' => $baseDir . '/database/migrations/2015_11_24_080521_create_products_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
