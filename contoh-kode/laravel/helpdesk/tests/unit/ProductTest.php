25f6542b70134f68984c1f9fe1d62b2555b17b03
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-25 16:24:04 +0700

    install codeception

---
+++ contoh-kode/laravel/helpdesk/tests/unit/ProductTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class ProductTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testFindById()
+    {
+        $product = App\Product::find(1);
+        $this->tester->seeRecord('products', [
+            'id' => $product->id,
+            'code' => $product->code
+        ]);
+    }
+}
