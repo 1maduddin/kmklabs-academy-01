f1aa56f03fd6983783e33b81a7bc5eeb8e7c2f5a
Endy Muhardin <endy.muhardin@gmail.com>
2015-11-30 10:38:07 +0700

    membuat html form

--- contoh-kode/laravel/helpdesk/resources/views/product/create.blade.php
+++ contoh-kode/laravel/helpdesk/resources/views/product/create.blade.php
@@ -6,4 +6,26 @@
 <h1>Create New Product</h1>
 
 
+{!! Form::open([
+    'route' => 'product.store'
+]) !!}
+
+<div class="form-group">
+    {!! Form::label('code', 'Code:', ['class' => 'control-label']) !!}
+    {!! Form::text('code', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('name', 'Name:', ['class' => 'control-label']) !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+</div>
+
+{!! Form::submit('Create New Product', ['class' => 'btn btn-primary']) !!}
+{!! Form::close() !!}
+
 @endsection
