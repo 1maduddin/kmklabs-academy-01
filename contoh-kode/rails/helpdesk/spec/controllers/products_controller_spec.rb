56617458b7fe676349372ab157e0d52f87e7e938
Endy Muhardin <endy.muhardin@gmail.com>
2015-12-02 15:29:46 +0700

    contoh penggunaan mock sederhana

--- contoh-kode/rails/helpdesk/spec/controllers/products_controller_spec.rb
+++ contoh-kode/rails/helpdesk/spec/controllers/products_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe ProductsController, type: :controller do
+describe ProductsController do
+
+    describe "index" do
+        productClass = class_double("Product")
+        expect(productClass).to receive(:paginate).with(page: 2, per_page: 10)
+
+        get :index, 'page' => '2'
+        # expect(response).to render_template("products/index")
+    end
 
 end
